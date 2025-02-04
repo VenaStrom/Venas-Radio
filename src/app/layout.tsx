4d28e5ecbd9d059e84886094c91dcf62cf41b2ce
viggoStrom <viggostrom05@gmail.com>
2025-02-04 16:17:44 +0100

    more setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,10 @@
+import "./global.scss";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "VR Radiospelare",
+    description: "En radiospelare gjord av och till för Viggo.",
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
