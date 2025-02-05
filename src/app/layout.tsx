ec24ecb8ed51112fdeacefa749afe0c526c54c07
viggoStrom <viggostrom05@gmail.com>
2025-02-05 10:06:37 +0100

    moved audio player

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import type { Metadata } from "next";
 import * as Icon from "lucide-react";
 import Link from "next/link";
+import AudioPlayer from "@/components/audio-player";
 
 export const metadata: Metadata = {
     title: "VR Radiospelare",
@@ -33,32 +34,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                 {children}
 
                 <footer className="pb-4 bg-zinc-950 flex flex-col self-end items-center gap-y-3">
-
-                    {/* Progress bar */}
-                    <div id="progress-bar" className="h-1 w-full bg-zinc-800 flex flex-col justify-start items-start">
-                        <div id="progress-bar-fill" className="h-full bg-zinc-100 w-[50%] rounded-e-sm"></div>
-                    </div>
-
-                    {/* Controls */}
-                    <div id="player" className="flex flex-col items-center w-full px-5">
-
-                        {/*
-                        <button>
-                            <Icon.SkipBack size={30} />
-                        </button>
-                        */}
-
-                        <button className="self-end">
-                            <Icon.Play size={30} className="fill-zinc-100" />
-                            {/* <Icon.Pause size={30} /> */}
-                        </button>
-
-                        {/*
-                        <button>
-                            <Icon.SkipForward size={30} />
-                        </button>
-                        */}
-                    </div>
+                    <AudioPlayer />
 
                     {/* Navigation Buttons */}
                     <nav className="w-2/3 flex flex-row justify-between items-center">
---
+++ src/components/audio-player.tsx
@@ -0,0 +1,31 @@
+import * as Icon from "lucide-react";
+
+export default function AudioPlayer() {
+    return (<>
+        {/* Progress bar */}
+        <div id="progress-bar" className="h-1 w-full bg-zinc-800 flex flex-col justify-start items-start">
+            <div id="progress-bar-fill" className="h-full bg-zinc-100 w-[50%] rounded-e-sm"></div>
+        </div>
+
+        {/* Controls */}
+        <div id="player" className="flex flex-col items-center w-full px-5">
+
+            {/*
+            <button>
+                <Icon.SkipBack size={30} />
+            </button>
+            */}
+
+            <button className="self-end">
+                <Icon.Play size={30} className="fill-zinc-100" />
+                {/* <Icon.Pause size={30} /> */}
+            </button>
+
+            {/*
+            <button>
+                <Icon.SkipForward size={30} />
+            </button>
+            */}
+        </div>
+    </>)
+}
