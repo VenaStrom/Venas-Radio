fa5a7162f1ca32b940d8ab3d7f4ca18a8c377db4
viggoStrom <viggostrom05@gmail.com>
2025-02-20 17:18:06 +0100

    remove broadcast parts of duration extraction

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,9 +3,15 @@ import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
 }
 
-export function extractDuration(episode: Episode) {
-  return episode.listenpodfile?.duration || episode.downloadpodfile?.duration || episode.broadcast?.broadcastfiles[0]?.duration || Infinity
+export function extractDuration(episode: Episode): number | null {
+    return (
+        episode.listenpodfile?.duration
+        ||
+        episode.downloadpodfile?.duration
+        ||
+        null
+    );
 }
