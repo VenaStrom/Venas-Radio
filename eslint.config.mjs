a68bfb7c858a55c1943c10ee9bbd34cb29cd8eaa
viggoStrom <viggostrom05@gmail.com>
2025-03-27 17:09:19 +0100

    update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -9,11 +9,15 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+/** @type {import("next/dist/server/config-shared").ESLintConfig} */
 const eslintConfig = [
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript"],
     rules: {
-      "@typescript-eslint/no-explicit-any": "off",
+      "no-unused-vars": [
+        "warning",
+        { "argsIgnorePattern": "^_", "varsIgnorePattern": "^_" }
+      ],
     }
   })
 ];
