6527bb411479e5c7077322974ed07344047e6d0a
viggoStrom <viggostrom05@gmail.com>
2025-03-28 22:36:46 +0100

    fix username uniqueness

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,8 +1,34 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { auth, clerkClient } from "@clerk/nextjs/server";
+import { randomUUID } from "node:crypto";
 
 const client = clerkClient();
+
+async function getUniqueUsername(baseUsername: string): Promise<string | null> {
+  const makeDigits = () => Math.floor(Math.random() * 10000).toString().padStart(4, "0");
+
+  let username = baseUsername;
+  let digits = "";
+  let isUnique = false;
+
+  while (!isUnique) {
+    // Check if the username exists
+    const existingUser = await prisma.user.findFirst({
+      where: { username },
+    });
+
+    if (!existingUser) {
+      isUnique = true;
+    } else {
+      // Try with digits
+      digits = makeDigits();
+      username = `${baseUsername}#${digits}`;
+    }
+  }
+
+  return username;
+}
 
 export async function POST(req: NextRequest) {
   try {
@@ -30,8 +56,21 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ error: "User not found" }, { status: 404 });
     }
 
-    // Existing user found
-    return NextResponse.json({}, { status: 200 });
+    // Create new user in db
+    const suggestedUsername = clerkUser.username || clerkUser.fullName || randomUUID();
+    const uniqueUsername = await getUniqueUsername(suggestedUsername);
+    if (!uniqueUsername) {
+      return NextResponse.json({ error: "Failed to generate a unique username" }, { status: 500 });
+    }
+    const newUser = await prisma.user.create({
+      data: {
+        id: userId,
+        username: uniqueUsername,
+      },
+    });
+
+    // Return new user
+    return NextResponse.json({ user: newUser }, { status: 200 });
 
   } catch (error) {
     console.error("Login error:", error);
