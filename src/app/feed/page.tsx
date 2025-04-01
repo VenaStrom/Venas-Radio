9ae86d67ce284d905380734d383af2939de3f346
viggoStrom <viggostrom05@gmail.com>
2025-04-01 09:32:41 +0200

    WIP episode fetcher on feed page

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,5 +1,18 @@
 import { prisma } from "@/lib/prisma";
 import { currentUser } from "@clerk/nextjs/server";
+import { Program, User } from "@prisma/client";
+
+function fetchEpisodes(programId: string, config: User) {
+  // Fetch episodes
+  const fromDate = new Date(Date.now() - config.fetchSpan * 86400000);
+  const toDate = new Date(Date.now() + 1 * 86400000); // 1 day ahead to include today
+  const toFormattedDate = (date: Date) => date.toISOString()
+  // const makeURL = (programId: string) => `${process.env.API_EPISODE_URL}?programid=${programId}&${process.env.API_COMMON_ARGS}`;
+  // const fetchURLs = user.programs.map((program) => makeURL(program.id.toString()));
+  // console.debug(fetchURLs);
+
+  const args = `programid=${programId}&fromdate=${fromDate.toISOString()}&todate=${toDate.toISOString()}`;
+}
 
 export default async function FeedPage() {
   const userId = (await currentUser())?.id;
@@ -8,11 +21,21 @@ export default async function FeedPage() {
   const dbUser = await prisma.user.findUnique({
     where: { id: userId },
     include: {
-      programs: true,
+      programs: {
+        include: { episodes: true },
+      },
     },
   });
 
+  if (!dbUser) return <main>User not found</main>;
+
+  dbUser.programs.forEach((program) => {
+    if (!program.episodes.length) {
+      console.info(`No episodes for program ${program.name}`);
+    }
+  });
+
   return (<main>
-    {JSON.stringify(dbUser, null, 2)}
+    inloggad
   </main>);
 }
