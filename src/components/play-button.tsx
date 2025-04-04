60eff7ea6f75ea0857c44d0f4b8411344be19bd2
viggoStrom <viggostrom05@gmail.com>
2025-04-04 19:04:43 +0200

    update play button

--- src/components/play-button.tsx
+++ src/components/play-button.tsx
@@ -8,8 +8,13 @@ import { useState } from "react";
 export function PlayButton({ episode }: { episode: Episode }) {
   const [buttonState, setButtonState] = useState<"playing" | "paused">("paused");
 
+  // Toggle on click
+  const toggleButton = () => {
+    setButtonState((prevState) => (prevState === "playing" ? "paused" : "playing"));
+  };
+
   return (
-    <Button variant={"link"} className="!p-0 !m-1 size-5 hover:fill-zinc-100/50">
+    <Button onClick={toggleButton} variant={"link"} className="!p-0 !m-1 size-5 hover:fill-zinc-100/50">
       {buttonState === "paused" ?
         <Icon.Play className="fill-zinc-100 size-full" />
         :
