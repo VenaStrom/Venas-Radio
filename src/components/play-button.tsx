90a2fa155c3c4ac3308406802ecd24bfc0db5f3a
viggoStrom <viggostrom05@gmail.com>
2025-04-04 18:16:49 +0200

    update play button

--- src/components/play-button.tsx
+++ src/components/play-button.tsx
@@ -1,9 +1,20 @@
+"use client";
+
 import { Episode } from "@/types";
 import { Button } from "@shadcn/button";
+import * as Icon from "lucide-react";
+import { useState } from "react";
 
 export function PlayButton({ episode }: { episode: Episode }) {
+  const [buttonState, setButtonState] = useState<"playing" | "paused">("paused");
 
   return (
-    Button
+    <Button variant={"link"} className="!p-0 !m-1 size-5 hover:fill-zinc-100/50">
+      {buttonState === "paused" ?
+        <Icon.Play className="fill-zinc-100 size-full" />
+        :
+        <Icon.Pause className="fill-zinc-100 size-full" />
+      }
+    </Button>
   );
 }
