169e4de59b589ce58721269cc342abc4f1acc712
viggoStrom <viggostrom05@gmail.com>
2025-04-05 12:11:15 +0200

    update slider

--- src/components/audio-player.tsx
+++ src/components/audio-player.tsx
@@ -17,8 +17,9 @@ export function AudioPlayer({ className = "", packet }: { className?: string, pa
     }
   }
 
+  /** Separate the actual and visual values of the slider. This is to avoid it being really difficult to reach edge values on mobile. */
   const sliderMargin = 9;
-  const calculateClampedValues = (value: number): { actual: number, visual: number } => {
+  const calculateValues = (value: number): { actual: number, visual: number } => {
     const clamped = Math.max(0, Math.min(value, 100));
     if (clamped === 100) return { actual: 100, visual: 100 + sliderMargin };
     if (clamped === 0) return { actual: 0, visual: -sliderMargin };
@@ -27,14 +28,14 @@ export function AudioPlayer({ className = "", packet }: { className?: string, pa
 
   const audioRef = useRef<HTMLAudioElement>(null);
   const [percent, setPercent] = useState<number>((packet.progress / packet.duration) * 100);
-  const [sliderValue, setSliderValue] = useState<number>(calculateClampedValues(percent).visual);
+  const [sliderValue, setSliderValue] = useState<number>(calculateValues(percent).visual);
 
+  const handleValueChange = useCallback((value: number[]) => setSliderValue(value[0]), []);
   const handleValueCommit = useCallback((value: number[]) => {
-    // The value we wanna get out is between 0 and 100
-    const { actual, visual } = calculateClampedValues(value[0]);
+    const { actual, visual } = calculateValues(value[0]);
     setSliderValue(visual);
     setPercent(actual);
-  }, [calculateClampedValues]);
+  }, [calculateValues]);
 
   return (
     <div className="w-full flex flex-col">
@@ -44,20 +45,19 @@ export function AudioPlayer({ className = "", packet }: { className?: string, pa
           *[data-slot='slider-thumb'] {
             z-index: 10;
             height: 64px;
-            width: 64px;
+            width: 96px;
             overflow: visible;
             opacity: 0;
           }`}
         </style>
 
         <Slider
-          // defaultValue={[percent]}
           value={[sliderValue]}
           min={-sliderMargin}
           max={100 + sliderMargin}
           className="**:rounded-none"
           onValueCommit={handleValueCommit}
-          onValueChange={value => setSliderValue(value[0])}
+          onValueChange={handleValueChange}
         />
       </div>
 
