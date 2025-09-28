943d522f99189f79eb562709213013447a4ca1ae
Vena <strom.vena+gh@gmail.com>
2025-09-28 23:53:47 +0200

    update settings panel

--- src/components/settings-menu.tsx
+++ src/components/settings-menu.tsx
@@ -9,63 +9,72 @@ import { SettingsIcon } from "lucide-react";
 import { useState } from "react";
 
 export default function SettingsMenu() {
-    const { settings, setAllSettings } = useSettingsStore();
-    const [uncommittedSettings, setUncommittedSettings] = useState({ ...settings, programIDsString: settings.programIDs.join(", ") });
+  const { settings, setAllSettings } = useSettingsStore();
+  const [uncommittedSettings, setUncommittedSettings] = useState({ ...settings, programIDsString: settings.programIDs.join(", ") });
 
-    return (
-        <Dialog onOpenChange={(isOpen) => {
-            // Save on close
-            if (isOpen) {
-                setUncommittedSettings({ ...settings, programIDsString: settings.programIDs.join(", ") });
-            }
-            else if (!isOpen) {
-                setAllSettings({ ...uncommittedSettings, programIDs: uncommittedSettings.programIDsString.split(",").map((id) => parseInt(id.trim())).filter((id) => !isNaN(id)) });
-                // Reload page
-                window.location.reload();
-            }
-        }}>
-            <DialogTrigger>
-                <SettingsIcon />
-            </DialogTrigger>
+  return (
+    <Dialog onOpenChange={(isOpen) => {
+      // Save on close
+      if (isOpen) {
+        setUncommittedSettings({ ...settings, programIDsString: settings.programIDs.join(", ") });
+      }
+      else if (!isOpen) {
+        setAllSettings({ ...uncommittedSettings, programIDs: uncommittedSettings.programIDsString.split(",").map((id) => parseInt(id.trim())).filter((id) => !isNaN(id)) });
+        // Reload page
+        window.location.reload();
+      }
+    }}>
+      <DialogTrigger>
+        <SettingsIcon />
+      </DialogTrigger>
 
-            <DialogContent className="w-[90%] bg-zinc-900 rounded-lg border-none">
-                <DialogHeader>
-                    <DialogTitle>
-                        Inställningar
-                    </DialogTitle>
-                    <DialogDescription>
+      <DialogContent className="w-[90%] bg-zinc-900 rounded-lg border-none">
+        <DialogHeader>
+          <DialogTitle>
+            Inställningar
+          </DialogTitle>
+          <DialogDescription>
 
-                    </DialogDescription>
-                </DialogHeader>
+          </DialogDescription>
+        </DialogHeader>
 
-                <div className="flex flex-col gap-y-2">
-                    {/* Back day */}
-                    <Label htmlFor="days-fetch-back">Antal dagar tillbaka program ska hämtas ifrån.</Label>
-                    <Input className="mb-4" id="days-fetch-back" placeholder="Antal dagar ex. 7" type="number" autoFocus={false}
-                        value={uncommittedSettings.fetchBack || ""}
-                        onChange={(e) => setUncommittedSettings({ ...uncommittedSettings, fetchBack: Math.max(0, parseInt(e.target.value)) })}
-                    />
+        <div className="flex flex-col gap-y-2">
+          {/* Compact view */}
+          <Label className="leading-4 flex flex-row gap-x-2 items-center mb-4" htmlFor="compact-view">
+            <Input className="h-5 w-fit" id="compact-view" type="checkbox" autoFocus={false}
+              checked={uncommittedSettings.compactView}
+              onChange={(e) => setUncommittedSettings({ ...uncommittedSettings, compactView: e.target.checked })}
+            />
+            Kompakt vy.
+          </Label>
 
-                    {/* Followed programs */}
-                    <Label className="leading-4" htmlFor="followed-programs">Program att hämta. <span className="text-zinc-400">OBS! Detta är inte den slutgiltiga lösningen för den här funktionen.</span></Label>
-                    <Input id="followed-programs" placeholder="Program ID ex. 4923, 178" type="text" autoFocus={false}
-                        value={uncommittedSettings.programIDsString || ""}
-                        onChange={(e) => setUncommittedSettings({ ...uncommittedSettings, programIDsString: e.target.value })}
-                    />
-                </div>
+          {/* Back day */}
+          <Label htmlFor="days-fetch-back">Antal dagar tillbaka program ska hämtas ifrån.</Label>
+          <Input className="mb-4" id="days-fetch-back" placeholder="Antal dagar ex. 7" type="number" autoFocus={false}
+            value={uncommittedSettings.fetchBack || ""}
+            onChange={(e) => setUncommittedSettings({ ...uncommittedSettings, fetchBack: Math.max(0, parseInt(e.target.value)) })}
+          />
 
-                <DialogFooter>
-                    <Button className="bg-zinc-950 text-zinc-100 mt-1 text-base p-5 font-semibold" autoFocus={true}
-                        onClick={(e) => {
-                            setAllSettings(uncommittedSettings);
-                            (e.target as HTMLButtonElement).textContent = "Sparat";
-                        }}
-                        onBlur={(e) => (e.target as HTMLButtonElement).textContent = "Spara"}
-                    >
-                        Spara
-                    </Button>
-                </DialogFooter>
-            </DialogContent>
-        </Dialog>
-    );
+          {/* Followed programs */}
+          {/* <Label className="leading-4" htmlFor="followed-programs">Program att hämta. <span className="text-zinc-400">OBS! Detta är inte den slutgiltiga lösningen för den här funktionen.</span></Label>
+          <Input id="followed-programs" placeholder="Program ID ex. 4923, 178" type="text" autoFocus={false}
+            value={uncommittedSettings.programIDsString || ""}
+            onChange={(e) => setUncommittedSettings({ ...uncommittedSettings, programIDsString: e.target.value })}
+          /> */}
+        </div>
+
+        <DialogFooter>
+          <Button className="bg-zinc-950 text-zinc-100 mt-1 text-base p-5 font-semibold" autoFocus={true}
+            onClick={(e) => {
+              setAllSettings(uncommittedSettings);
+              (e.target as HTMLButtonElement).textContent = "Sparat";
+            }}
+            onBlur={(e) => (e.target as HTMLButtonElement).textContent = "Spara"}
+          >
+            Spara
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
 }
