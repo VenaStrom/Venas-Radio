590c6854cea7a6534ec4ae66f82de93f6ad0e1c1
Vena <strom.vena+gh@gmail.com>
2025-09-29 12:41:27 +0200

    reactify progress bar

--- src/components/progress-bar.tsx
+++ src/components/progress-bar.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from "react";
+
 /**
  * A progress bar component
  * @param progress - The progress of the bar, in percent. e.g. 15, not 0.15"
@@ -7,18 +9,16 @@
  * @example <ProgressBar progress={51} className="col-span-2 rounded-sm" />
  */
 export default function ProgressBar({ progress, className, innerClassName }: { progress: number | string, className?: string, innerClassName?: string }) {
-    // Offset the visual progress by 3% to account for the rounded corners and readability
+  // Offset the visual progress by 3% to account for the rounded corners and readability
 
-    if (parseFloat(progress.toString()) > 0) {
-        const offset = 3;
-        const clamped = Math.min(100, Math.max(0, Number(progress)));
-        const normalized = clamped / 100 * (100 - offset);
-        progress = offset + normalized;
-    }
+  const offset = 3;
+  const clamped = useMemo(() => Math.min(100, Math.max(0, Number(progress))), [progress]);
+  const normalized = useMemo(() => clamped / 100 * (100 - offset), [clamped]);
+  progress = useMemo(() => offset + normalized, [normalized]);
 
-    return (
-        <div className={`h-1 w-full bg-zinc-800 flex flex-col justify-start items-start ${className || ""}`}>
-            <div className={`h-full bg-zinc-100 rounded-e-sm ${innerClassName}`} style={{ width: progress + "%" }}></div>
-        </div>
-    )
+  return (
+    <div className={`h-1 w-full bg-zinc-800 flex flex-col justify-start items-start ${className || ""}`}>
+      <div className={`h-full bg-zinc-100 rounded-e-sm ${innerClassName}`} style={{ width: progress + "%" }}></div>
+    </div>
+  )
 }
