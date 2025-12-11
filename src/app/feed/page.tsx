5086a98c6d5a8b179902838a8dc5dd804f1324aa
VenaStrom <strom.vena+gh@gmail.com>
2025-12-11 13:32:09 +0100

    update time formatter

--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -25,23 +25,43 @@ export default function FeedPage() {
           </>
         ) : (
           episodes.map((episode, i) => {
-            const dom = <EpisodeDOM episode={episode} key={episode.id} />;
+            const bareEpisode = <EpisodeDOM episode={episode} key={episode.id} />;
 
             const thisDate = episode.publishDate.toISOString().slice(0, 10);
             const prevDate = i > 0 ? episodes[i - 1].publishDate.toISOString().slice(0, 10) : null;
 
-            if (thisDate === prevDate) return dom;
+            if (thisDate === prevDate) return bareEpisode; // No day shift
+
+            let dateHeader = "";
+
+            const today = new Date().toISOString().slice(0, 10)
+
+            const allowedRelativeDays = [-2, -1, 0, 1, 2];
+
+            const relativeTimeFormatter = new Intl.RelativeTimeFormat("sv-SE", { numeric: "auto", style: "narrow" });
+
+            const dayDiff = (new Date(thisDate).getTime() - new Date(today).getTime()) / (1000 * 60 * 60 * 24);
+
+            if (allowedRelativeDays.includes(dayDiff)) {
+              dateHeader = relativeTimeFormatter.format(dayDiff, "day");
+            }
+            else {
+              dateHeader = episode.publishDate.toLocaleDateString(
+                "sv-SE", { weekday: "long", month: "short", day: "numeric" }
+              );
+            }
+
+            const furthestFutureRelativeDay = allowedRelativeDays.reduce((a, b) => Math.max(a, b), -Infinity);
+            if (dayDiff > furthestFutureRelativeDay) {
+              dateHeader += " (tidig publicering)";
+            }
 
             return (
               <React.Fragment key={episode.id + "-fragment"}>
                 <li className="w-full -mb-6 text-sm text-center text-zinc-400" key={"date-header-" + thisDate + "-" + prevDate}>
-                  {thisDate === new Date().toISOString().slice(0, 10)
-                    ? "Idag"
-                    : thisDate === new Date(new Date().setDate(new Date().getDate() - 1)).toISOString().slice(0, 10)
-                      ? "Igår"
-                      : episode.publishDate.toLocaleDateString("sv-SE", { weekday: "long", month: "short", day: "numeric" })}
+                  {dateHeader}
                 </li>
-                {dom}
+                {bareEpisode}
               </React.Fragment>
             );
           })
