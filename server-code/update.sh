10f216caaac4a3cd04197f2e2b0af2bf5a22c430
VenaStrom <strom.vena+gh@gmail.com>
2026-02-27 10:08:34 +0100

    add chown to root .nvm

--- server-code/update.sh
+++ server-code/update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
- 
+
 cd /root/Venas-Radio
- 
+
 git fetch --all
 git checkout origin/main --force
 sudo systemctl daemon-reload
- 
+
 # Build
 yarn install --frozen-lockfile
 yarn prisma generate
@@ -13,9 +13,10 @@ yarn build
 
 # Make /server-code executable
 chmod +x server-code/*
- 
+
 # Allow vr to read /root/Venas-Radio
 sudo chown -R vr:vr /root/Venas-Radio
- 
+sudo chown -R vr:vr /root/.nvm
+
 # Restart the service
 sudo systemctl restart vr-radio-start
