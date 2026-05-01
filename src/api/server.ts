5b4fd52b5fd8cee0a01d6e98d7c1fd587d59adac
VenaStrom <strom.vena+gh@gmail.com>
2026-05-01 11:23:06 +0200

    add file serving to express

--- src/api/server.ts
+++ src/api/server.ts
@@ -3,9 +3,10 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-app.get("/api/hello", (_req, res) => {
-  res.json({ message: "Hello, World!" });
-});
+// Static app
+app.get("/", (_req, res) => { res.sendFile("index.html", { root: "dist" }); });
+app.get("/assets/:file", (_req, res) => { res.sendFile(`assets/${_req.params.file}`, { root: "dist" }); });
+
 
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
