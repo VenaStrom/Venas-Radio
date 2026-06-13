6b5ab670fe74d73ec0e0f95b0322986700957c8d
VenaStrom <strom.vena+gh@gmail.com>
2026-06-13 18:21:15 +0200

    new eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,70 +1,115 @@
-import {Config, defineConfig, globalIgnores } from "eslint/config";
+import type { Config } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import nextTS from "eslint-config-next/typescript";
 import nextVitals from "eslint-config-next/core-web-vitals";
-import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
-const nonAppTsBaseConfig = tseslint.configs.recommendedTypeChecked;
+const tsBaseConfig = tseslint.configs.recommendedTypeChecked;
+const nextBaseConfig = [...nextTS, ...nextVitals];
 
-const tsCommonRules: Config["rules"] = {
-  "prefer-const": "warn",
+const commonRules: Config["rules"] = {
+  "comma-dangle": ["error", "always-multiline"],
+  "default-case-last": "error",
+  "default-case": "error",
+  "eqeqeq": ["error", "smart"],
+  "no-case-declarations": "error",
+  "no-cond-assign": ["error", "always"],
+  "no-duplicate-imports": ["error", { allowSeparateTypeImports: true, includeExports: true }],
+  "no-empty": "warn",
+  "no-eval": "error",
+  "no-lonely-if": "warn",
+  "no-multi-assign": "error",
+  "no-multi-str": "error",
+  "no-restricted-globals": "error",
+  "no-self-compare": "error",
+  "no-sequences": "error",
+  "no-template-curly-in-string": "warn",
+  "no-throw-literal": "error",
+  "no-unmodified-loop-condition": "error",
+  "no-unreachable-loop": "error",
+  "no-useless-assignment": "warn",
+  "no-useless-rename": "error",
+  "no-var": "error",
+  "no-with": "error",
+  "prefer-const": "error",
+  "radix": "error",
+  "semi": ["error", "always"],
+  // "no-alert": "warn", // TODO: cleanse this repo
+  // "no-magic-numbers": "error", // TODO: cry
+  // "no-param-reassign": "error", // TODO: implement
+  // "no-return-assign": "error", // I don't wanna deal with the 4 arrow functions using this right now
+  // "no-warning-comments": "warn", // TODO: implement hihi
+  // "quotes": ["error", "double", { avoidEscape: true, allowTemplateLiterals: true }], // Authoritarian option :))
+  // "require-atomic-updates": "error", // TODO consider this rule
+  // "sort-imports": "warn", // Large touch of code base to implement, also does not auto fix most cases which is sad. Probably will never use.
   "@typescript-eslint/no-unused-vars": [
     "error",
     {
-      "args": "all",
-      "argsIgnorePattern": "^_",
-      "caughtErrors": "all",
-      "caughtErrorsIgnorePattern": "^_",
-      "destructuredArrayIgnorePattern": "^_",
-      "varsIgnorePattern": "^_",
-      "ignoreRestSiblings": true
+      args: "all",
+      argsIgnorePattern: "^_",
+      caughtErrors: "all",
+      caughtErrorsIgnorePattern: "^_",
+      destructuredArrayIgnorePattern: "^_",
+      varsIgnorePattern: "^_",
+      ignoreRestSiblings: true,
     },
   ],
+  "@typescript-eslint/ban-ts-comment": "error",
+  "@typescript-eslint/class-methods-use-this": "error",
+  "@typescript-eslint/consistent-type-assertions": "error",
+  "@typescript-eslint/consistent-type-definitions": ["warn", "type"],
+  "@typescript-eslint/consistent-type-exports": "warn",
+  "@typescript-eslint/consistent-type-imports": "warn",
+  "@typescript-eslint/no-array-constructor": "error",
+  "@typescript-eslint/no-base-to-string": "warn",
+  "@typescript-eslint/no-confusing-non-null-assertion": "error",
+  "@typescript-eslint/no-empty-function": "warn",
+  "@typescript-eslint/no-explicit-any": "error",
+  "@typescript-eslint/no-floating-promises": "warn",
+  "@typescript-eslint/no-for-in-array": "error",
+  "@typescript-eslint/no-implied-eval": "error",
+  "@typescript-eslint/no-import-type-side-effects": "error",
+  "@typescript-eslint/no-misused-promises": "warn",
+  "@typescript-eslint/no-non-null-assertion": "warn",
+  "@typescript-eslint/no-restricted-imports": ["error", "fs", "path", "crypto", "child_process", "os", "http"],
+  "@typescript-eslint/no-unnecessary-type-assertion": "warn",
   "@typescript-eslint/no-unsafe-argument": "warn",
   "@typescript-eslint/no-unsafe-assignment": "warn",
   "@typescript-eslint/no-unsafe-call": "warn",
-  "@typescript-eslint/no-unsafe-return": "error",
-  "@typescript-eslint/no-unsafe-member-access": "warn",
   "@typescript-eslint/no-unsafe-enum-comparison": "warn",
-  "@typescript-eslint/no-non-null-assertion": "warn",
-  "@typescript-eslint/no-confusing-non-null-assertion": "error",
-  "@typescript-eslint/require-await": "warn",
-  "@typescript-eslint/no-misused-promises": "warn",
-  "@typescript-eslint/no-floating-promises": "warn",
-  "@typescript-eslint/no-unnecessary-type-assertion": "warn",
+  "@typescript-eslint/no-unsafe-member-access": "warn",
+  "@typescript-eslint/no-unsafe-return": "error",
+  "@typescript-eslint/no-use-before-define": ["error", { functions: false, classes: false }],
   "@typescript-eslint/only-throw-error": "warn",
-  "@typescript-eslint/switch-exhaustiveness-check": "warn",
-  "@typescript-eslint/consistent-type-imports": "warn",
-  "@typescript-eslint/consistent-type-exports": "warn",
-  "@typescript-eslint/consistent-type-definitions": ["warn", "type"],
-  "@typescript-eslint/ban-ts-comment": "error",
-  "@typescript-eslint/prefer-nullish-coalescing": ["warn", { ignorePrimitives: { string: true, boolean: true, }, },],
+  "@typescript-eslint/prefer-nullish-coalescing": ["warn", { ignorePrimitives: { string: true, boolean: true } }],
   "@typescript-eslint/prefer-optional-chain": "warn",
+  "@typescript-eslint/require-await": "warn",
   "@typescript-eslint/restrict-template-expressions": "warn",
-  "@typescript-eslint/no-base-to-string": "warn",
+  "@typescript-eslint/switch-exhaustiveness-check": "warn",
   "@typescript-eslint/use-unknown-in-catch-callback-variable": "error",
-  "@typescript-eslint/consistent-type-assertions": "error",
-  "@typescript-eslint/no-for-in-array": "error",
-  "@/no-useless-assignment": "warn",
-  "eqeqeq": ["error", "smart"],
-  "semi": ["error", "always"],
-  "comma-dangle": ["error", "always-multiline",], // Would be nice but not tweakable enough
+  // "@typescript-eslint/array-type": ["error", { default: "array", readonly: "generic" }], // could be nice but cannot be bother to manually fix all occurrences
 };
 
 export default defineConfig([
   { // App linting
-    ...reactRefresh.configs.next,
     name: "App src/",
     files: ["src/**/*.{ts,tsx}"],
     extends: [
-      ...nextTS,
-      ...nextVitals,
+      ...nextBaseConfig,
     ],
     rules: {
-      "react-hooks/set-state-in-effect": "warn",
-      "react-hooks/set-state-in-render": "warn",
-      "react-hooks/immutability": "warn", // This should probably be a warning but the current recipe pipeline is dependant on it :sweat_smile:
-      ...tsCommonRules,
+      "react-hooks/set-state-in-effect": "off", // TODO: get a grip and understand react
+      "react-hooks/set-state-in-render": "off", // TODO: get a grip and understand react
+      "react-hooks/immutability": "error",
+      "react/button-has-type": "error",
+      "react/checked-requires-onchange-or-readonly": "error",
+      "react/jsx-boolean-value": ["error", "always"],
+      "react/jsx-no-leaked-render": "error",
+      "react/jsx-no-target-blank": "error",
+      "react/jsx-no-useless-fragment": "error",
+      "react/jsx-pascal-case": "error",
+      "react/no-array-index-key": "off", // In a perfect world...
+      ...commonRules,
     },
     languageOptions: {
       parserOptions: {
@@ -77,44 +122,46 @@ export default defineConfig([
     name: "Tests tests/",
     files: ["tests/**/*.{ts,tsx}"],
     extends: [
-      ...nonAppTsBaseConfig,
+      ...tsBaseConfig,
     ],
     rules: {
-      ...tsCommonRules,
+      ...commonRules,
     },
     languageOptions: {
       parserOptions: {
-        project: ["./tsconfig.json"],
+        project: "./tsconfig.json",
         tsconfigRootDir: process.cwd(),
       },
     },
   },
   { // Script linting
     name: "scripts scripts/",
-    files: ["scripts/**/*.{ts,tsx}"],
+    files: ["scripts/**/*.{ts,tsx}", "*.config.ts"],
     extends: [
-      ...nonAppTsBaseConfig,
+      ...tsBaseConfig,
     ],
     rules: {
-      ...tsCommonRules,
+      ...commonRules,
     },
     languageOptions: {
       parserOptions: {
-        project: ["./tsconfig.json"],
+        project: "./tsconfig.json",
         tsconfigRootDir: process.cwd(),
       },
     },
   },
   globalIgnores([
+    ".prisma/**/*",
     "src/prisma/generated/**/*",
+    "src/.prisma/**/*",
+    ".prisma/**/*",
+    "prisma/generated/**/*",
     "node_modules/**/*",
-    "prisma/**/*",
     ".next/**/*",
     "out/**/*",
     "dist/**/*",
     "build/**/*",
     "ignore/**/*",
     "next-env.d.ts",
-    "src/components/ui/**/*", // Shadcn UI components don't follow nobody's rules :sweat_smile:
   ]),
 ]);
